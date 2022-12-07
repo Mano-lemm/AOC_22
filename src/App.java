bdd05868ed8d7c289a4dda41ed1fe1c0ee3c29b9
Mano-lemm <lemmensmano@gmail.com>
2022-12-07 08:57:52 +0100

    rel path

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        day6.solve2("/home/mano/intec/AOC_22/src/input/day6.txt");
+        day6.solve2("src/input/day6.txt");
     }
 }
